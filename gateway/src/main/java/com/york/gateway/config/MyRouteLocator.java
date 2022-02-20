4eb49eabf15b0086cc89ec74e8c4db73c2b1ba37
v-chenyq29 <v-chenyq29@vanke.com>
2022-02-20 10:33:24 +0800

    修改

---
+++ gateway/src/main/java/com/york/gateway/config/MyRouteLocator.java
@@ -0,0 +1,21 @@
+package com.york.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRouteLocator {
+
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+}
